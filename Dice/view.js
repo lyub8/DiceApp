7734d2d165eda270f7f22399206bc88d11c9b1a2
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-02-02 18:10:30 +0000

    view done

--- Dice/view.js
+++ Dice/view.js
@@ -3,6 +3,7 @@
 fucntion view() {
     var image  = document.getElementById("dice");
     this.showFace = function (faceNo) {
-
+        image.src = "images/" + faceNo + ".png";
+        image.alt = faceNo;
     };
 }
