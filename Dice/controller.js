84a8bf7253a401e78ed16a4502ebd2fd6421b4db
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-02-02 18:16:33 +0000

    controller in build

---
+++ Dice/controller.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var diceView = new view(), diceModel = new model(), diceController = null;
+
+function controller () {
+
+    this.updateDisplay = function () {
+        diceView = 
+    };
+}
--- Dice/view.js
+++ Dice/view.js
@@ -2,8 +2,13 @@
 
 fucntion view() {
     var image  = document.getElementById("dice");
+    var randomBtn = document.getElementById("rndB");
     this.showFace = function (faceNo) {
         image.src = "images/" + faceNo + ".png";
         image.alt = faceNo;
+    };
+
+    this.setRandomClickCallBack = function (callback) {
+        randomBtn.addEventListener("click", callback);
     };
 }
