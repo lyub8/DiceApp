dad0ee166f81ed8b1e7fdba7b280c3be93ac0ff9
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-02-02 18:19:41 +0000

    mvc done

--- Dice/controller.js
+++ Dice/controller.js
@@ -5,6 +5,16 @@ var diceView = new view(), diceModel = new model(), diceController = null;
 function controller () {
 
     this.updateDisplay = function () {
-        diceView = 
+        diceView.showFace(model.getFace());
+    };
+
+    this.init = function () {
+        diceView.setRandomClickCallBack( function () {
+            diceModel.randomize();
+            diceController.updateDisplay();
+        });
     };
 }
+
+diceController = new controller();
+window.addEventListener("load", controller.init());
